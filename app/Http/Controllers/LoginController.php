057d22cda67ed0531d67d16854c7edbcc6c04c2c
Daniela9903 <rosas.tome@gmail.com>
2022-03-07 12:53:44 -0600

    Creación modelo

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    //
+
+    function test(){
+        return "Funciones desde controller";
+    }
+
+    function Hola(){
+        return "Funciones PRUEBA";
+    }
+    
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// route::get('test',function(){
+//     return "Funciona la Api";
+// });
+
+route::get('testController',[LoginController::class,'test']);
+
+route::get('testController',[LoginController::class,'Hola']);
